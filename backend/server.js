2bb79a738e4fd674577dd5b5721eb6df8bdb045a
zahra <zahratee@gmail.com>
2014-10-18 23:49:35 +0100

    Created save to db method

--- backend/server.js
+++ backend/server.js
@@ -39,16 +39,16 @@ var Article = mongoose.model('Article', ArticleSchema);
 mongoose.connect('mongodb://localhost/test');
 
 
-/*function saveNewsToDb(json){
+function saveNewsToDb(json){
 
 	for(var i = 0; i < json.length; i++){
 		var obj = json[i];
 		var article = new Article({
 			id: obj.id,
-			picurl: String,
-			headline: String,
-			trailtext: String,
-			url: String;
+			picurl: extractImageURI(obj.fields.main),
+			headline: obj.fields.headline,
+			trailtext: obj.fields.trailtext,
+			url: obj.webUrl;
 		});
 
 		article.save(function(err, article) {
@@ -57,12 +57,11 @@ mongoose.connect('mongodb://localhost/test');
 		});
 	}
 
-}*/
-
+}
 
 
 function getAllNewsJson(req,res){
-	var data = request('http://content.guardianapis.com/search?api-key=t3myqd7scnfu4t5w8zp7jx4v&show-fields=headline,trailText&page-size=100', function (error, response, body) {
+	var data = request('http://content.guardianapis.com/search?api-key=t3myqd7scnfu4t5w8zp7jx4v&show-fields=headline,trailText,main&page-size=100', function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 		  	var jsonData = JSON.parse(body);
 		  	return jsonDataresponse.results;
@@ -81,7 +80,7 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.get('/getallnews', getAllNews);
+server.get('/getallnewsjson', getAllNewsJson);
 
 
 server.listen(8080, function() {
